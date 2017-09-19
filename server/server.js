44f87a986f8d6cebb01ee60ce2bd7bbb4ffbd35e
Brian <briankimx91@gmail.com>
2017-09-18 17:57:41 -0700

    server side started

--- server/server.js
+++ server/server.js
@@ -1,3 +1,16 @@
-const mysql = require("mysql");
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+
+
+const app = express();
+const PORT = 8000;
+
+
+app.use(express.static(path.resolve(__dirname, '..', 'client')));
+
+
+app.listen(PORT, function(){
+    console.log("We started on PORT: ", PORT);
+});
